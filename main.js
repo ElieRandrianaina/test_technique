b9fc12ee25bfc0e4f48f1f6c8432b02e3b9e5bda
Elie Randrianaina <randrianainaelie@gmail.com>
2024-05-11 10:06:13 +0200

    probleme 2 presque résolu !!

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 /** Problème 1 */
-
-isValidHtml = (html = new String, isValid = true) => {
+const isValidHtml = (html = new String, isValid = true) => {
+    
     html.startsWith("<") & html.endsWith("/>") ? isValid : isValid = false
     return isValid
 }
+
 /**cas de test */
-let test1 = isValidHtml("<>hello world<p/>")
-let test2 = isValidHtml("<>hello world<p/")
+let test1 = isValidHtml("<p>hello world<p/>")
+let test2 = isValidHtml("<p>hello world<p>")
 console.log(test1) // true
 console.log(test2) // false
 
-/**probleme 2 */
 
+/**=========================================================================================== */
+/**probleme 2 */
+const nonConsecutiveDigits = (... N) =>
+{
+    let prec,cur
+
+    for(cur of N){
+        if (prec > cur) {
+            prec = cur            
+            nonConsecutiveDigits(cur+1)
+        }
+    }
+    return prec
+}
+
+/** cas de test */
+let test3 = nonConsecutiveDigits(1765)
+console.log(test3)
+
+
+
+/**=========================================================================================== */
+/**probleme 3 */
+const pairProgramming = (experience = new Array, mostExeperienced = true)=>{
+
+}
